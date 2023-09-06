93c8d82859d308ced4dec595175ceacf950fd617
shikhar aditya <satyamshikhar@gmail.com>
2023-09-07 00:23:32 +0530

    added html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import mysql.connector
 from flask import request
+import requests
+import smtplib
+from email.mime.text import MIMEText
+
 cnx = mysql.connector.connect(user='satyam', password='satyam', host='127.0.0.1', database='mysql')
 cnx.close()
 app = Flask(__name__)
@@ -13,14 +17,46 @@ def hello_world():  # put application's code here
 
 @app.route('/add_subscriber', methods=['GET', 'POST'])
 def add_subscriber():
-    data = request.data
-    print(data)
+    a = request.form['fname']
+    b = request.form['email']
+    data = (a, b)
     cnx = mysql.connector.connect(user='satyam', password='satyam', host='127.0.0.1', database='mysql')
     cursor = cnx.cursor()
     insert = 'insert into subscribers (first_name,email) values(%s,%s);'
     cursor.execute(insert, data)
+    cnx.commit()
     cursor.close()
     cnx.close()
+    return {'statuscode': 200}
+
+
+@app.route('/unsubscribe', methods=['GET', 'POST'])
+def remove_subscriber():
+    a = request.form['email']
+    # a = (a)
+    cnx = mysql.connector.connect(user='satyam', password='satyam', host='127.0.0.1', database='mysql')
+    cursor = cnx.cursor()
+    up = "update subscribers set is_active = false where email = '{}';"
+    cursor.execute(up.format(a))
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+    return {'statuscode': 200}
+
+
+@app.route('/send_email')
+def send_simple_message():
+    msg = MIMEText('Testing some Mailgun awesomness')
+    msg['Subject'] = "Hello"
+    msg['From'] = "foo@sandboxe31f460a7b0d4ce8a327a5aee444d8c7.mailgun.org"
+    msg['To'] = "satyamshikhar@gmail.com"
+
+    s = smtplib.SMTP('smtp.mailgun.org', 587)
+
+    s.login('postmaster@sandboxe31f460a7b0d4ce8a327a5aee444d8c7.mailgun.org', '18be4751d126ff1317ab5f7e255c6c82-7ca144d2-004b0108')
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+    s.quit()
+    return {'statuscode': 200}
 
 
 if __name__ == '__main__':
