59cc1263e67aca91f66106475511a34e55583da6
shikhar aditya <satyamshikhar@gmail.com>
2023-09-06 14:28:34 +0530

    added db info

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import mysql.connector
 
+cnx = mysql.connector.connect(user='satyam', password='satyam',host='127.0.0.1',database='mysql')
+cnx.close()
 app = Flask(__name__)
 
 
