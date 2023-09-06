b427232f646c5720cce4859cc0209fda1f3ace43
shikhar aditya <satyamshikhar@gmail.com>
2023-09-06 22:47:31 +0530

    added html

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector
-
-cnx = mysql.connector.connect(user='satyam', password='satyam',host='127.0.0.1',database='mysql')
+from flask import request
+cnx = mysql.connector.connect(user='satyam', password='satyam', host='127.0.0.1', database='mysql')
 cnx.close()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    render_template('home.html')
+
+
+@app.route('/add_subscriber', methods=['GET', 'POST'])
+def add_subscriber():
+    data = request.data
+    cnx = mysql.connector.connect(user='satyam', password='satyam', host='127.0.0.1', database='mysql')
+    cursor = cnx.cursor()
+    insert = 'insert into subscribers (first_name,email) values(%s,%s);'
+    cursor.execute(insert, data)
+    cnx.close()
 
 
 if __name__ == '__main__':
